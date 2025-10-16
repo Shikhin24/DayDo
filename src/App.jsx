a69f6ca62d1880dfee49c672cf33bb1092b0ed80
Shikhin24 <shikhinrg246@gmail.com>
2025-10-16 11:41:27 +0530

    Taskform: added taskform functionality and localstorage integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import Taskform from "./Components/Taskform"
 import Tasklist from "./Components/Tasklist"
 import Progresstracker from "./Components/Progresstracker"
+import { useEffect, useState } from "react"
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
   return (
     <div>
       <h1>DayDo</h1>
       <p>Smart ToDo</p>
-      <Taskform />
+      <Taskform addTask={addTask}/>
       <Tasklist />
       <Progresstracker />
-      <button>Clear All</button>
+      <button>Clear</button>
     </div>
   )
 }
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,19 +1,40 @@
-export default function Taskform() {
-    return(
-        <form>
-            <div>
-                <input type="text" placeholder="Enter the task" />
-                <button type="submit">Add task</button>
-            </div>
+import { useState } from "react"
 
+export default function Taskform({addTask}) {
+    const [task, setTask] = useState('');
+    const [priority, setPriority] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(task.trim() === "") return;
+
+        
+        addTask({task: task, priority, category, completed: false});
+
+        //reset
+        setTask('');
+        setPriority('medium');
+        setCategory('general');
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
             <div>
-                <select>
+                <input type="text" placeholder="Enter the task" 
+                onChange={(e) => setTask(e.target.value)} value={task}/>
+                <button type="submit">Add task</button>
+            
+                <select onChange={(e) => setPriority(e.target.value)} value={priority}> 
+                    <option value="" disabled hidden>Priority</option>
                     <option value="high">high</option>
                     <option value="medium">medium</option>
                     <option value="low">low</option>
                 </select>
 
-                <select>
+                <select onChange={(e) => setCategory(e.target.value)} value={category}> 
+                    <option value="" disabled hidden>Category</option>
                     <option value="general">general</option>
                     <option value="work">work</option>
                     <option value="personal">personal</option>
